ab7d14d2ad953a05a8c951790d483b9ee5ced463
Jasma Tamang <02230213.cst@rub.edu.bt>
2024-02-14 19:54:20 +0530

    First commit

---
+++ Trial-01.py
@@ -0,0 +1,19 @@
+print("Hi, i am jasma")
+noun=input ("write the name of a person you care:")
+p_noun= input("Type: He/She")
+noun2=input("write you relationship with the peerson:")
+place=input("Choose a place")
+adjective=input("Choose an adjective(Describing word):")
+noun3=input("Choose a noun:")
+# Print a story from the user
+print("----------------------------------------------")
+print("I am very greatful to have",noun,'in my life')
+print(p_noun,"is someone whom I trust the most because",noun,'is my',noun2)
+
+print(p_noun,'is very', adjective, )
+
+print('Sometimes i feel like i dont deserve to be',p_noun,noun2,'but....')
+
+print('Well...lets keep it to that much for today.')
+
+print('------------------------------------------')
