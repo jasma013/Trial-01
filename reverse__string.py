6e3d7088dc603760430c932c7af007e69076e4bd
Jasma Tamang <02230213.cst@rub.edu.bt>
2024-05-20 19:34:55 +0530

    olleh-reversed

---
+++ reverse__string.py
@@ -0,0 +1,6 @@
+def reverse_string(s):
+    # Return the reversed string using slicing
+    return s[::-1]
+
+# Test the function by printing the reversed string of "hello"
+print(reverse_string("hello"))  # Output: "olleh"
