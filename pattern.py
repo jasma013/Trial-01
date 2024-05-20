bf237e046eed1b86f0d39e7bc46c62dac8fc6f86
Jasma Tamang <02230213.cst@rub.edu.bt>
2024-05-20 19:30:09 +0530

    asterisk

---
+++ pattern.py
@@ -0,0 +1,8 @@
+# Loop through numbers from 1 to 6 (inclusive)
+for i in range(1, 7):
+    # Initialize the variable x with an asterisk
+    x = "*"
+    # Multiply the asterisk by the current loop index to get the correct number of asterisks
+    x = x * i
+    # Print the resulting string of asterisks
+    print(f"{x}")
